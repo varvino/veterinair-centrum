f4afcb778c14a327be497adbc2d67554dfa37fa5
varvino <graafmans.tom@gmail.com>
2020-02-26 02:25:35 +0100

    Theme: added functions

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+// Get title function
+function the_title($post_type_url)
+{
+    $post_type = file_get_contents(ROOT . $post_type_url);
+    @$doc = new DOMDocument();
+    @$doc->loadHTML($post_type);
+    $xpath = new DomXPath($doc);
+    $nodeList = $xpath->query("//*[@class='blog__headline']");
+    $node = $nodeList->item(0);
+    $title = $node->nodeValue;
+
+    if (isset($post_type)) :
+        echo $title;
+    endif;
+}
+
+// Get excerpt function
+function the_excerpt($post_type_url)
+{
+    $post_type = file_get_contents(ROOT . $post_type_url);
+    @$doc = new DOMDocument();
+    @$doc->loadHTML($post_type);
+    $xpath = new DomXPath($doc);
+    $nodeList = $xpath->query("//*[@class='blog__content-lead']");
+    $node = $nodeList->item(0);
+    $excerpt = $node->nodeValue;
+
+    if (isset($post_type)) :
+        echo $excerpt;
+    endif;
+}
