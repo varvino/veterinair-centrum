f0adc42f029721ab90233665da4ca5cdc7c067ac
varvino <graafmans.tom@gmail.com>
2020-02-26 01:48:38 +0100

    Navigation: added interactivity to the navbar on mobile

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,11 +1,21 @@
-const button = document.querySelector(".js-hamburger");
-const sideNavButton = document.querySelector(".js-close-icon");
-const sidenav = document.querySelector(".js-sidenav");
+const button = document.querySelector(".js-hamburger")
+const sideNavButton = document.querySelector(".js-close-icon")
+const sidenav = document.querySelector(".js-sidenav")
+const navItems = document.querySelectorAll('.nav__item')
 
+// Toggle sidenav when hamburger icon has been clicked on
 button.addEventListener("click", () => {
-    sidenav.classList.toggle("js-sidenav--toggled");
-});
+  sidenav.classList.toggle("js-sidenav--toggled")
+})
 
+// Remove sidenav when close icon has been clicked on
 sideNavButton.addEventListener("click", () => {
-  sidenav.classList.remove("js-sidenav--toggled");
-});
+  sidenav.classList.remove("js-sidenav--toggled")
+})
+
+// Remove sidenav when nav items have been clicked on
+navItems.forEach(function (element) {
+  element.addEventListener('click', function (e) {
+    sidenav.classList.remove("js-sidenav--toggled")
+  })
+})
