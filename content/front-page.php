2a58041b865c3005736c644ed0a6c974fad7130e
varvino <graafmans.tom@gmail.com>
2020-02-26 02:50:55 +0100

    Front-page: added anchor id's, added root links

--- content/front-page.php
+++ content/front-page.php
@@ -1,99 +1,90 @@
-<div class="about-container">
-    <div class="about__carousel carousel full-width">
-        <div class="about__carousel-item">
-            <img src="./src/img/icons/icon_hospital.svg" alt="Kom naar onze dierenkliniek!" class="about__icon">
-            <p class="about__text">Wij zijn een dierenartsenpraktijk voor gezelschapsdieren, paarden en landbouwhuisdieren</p>
+<main>
+    <div class="about__outer-container" id="about">
+        <div class="about__inner-container">
+            <div class="about__carousel carousel">
+                <div class="about__carousel-item">
+                    <img src="<?php echo HTTP; ?>src/img/icons/icon_hospital.svg" alt="Kom naar onze dierenkliniek!" class="about__icon">
+                    <p class="about__text">Wij zijn een dierenartsenpraktijk voor gezelschapsdieren, paarden en landbouwhuisdieren</p>
+                </div>
+                <div class="about__carousel-item">
+                    <img src="<?php echo HTTP; ?>src/img/icons/icon_blog.svg" alt="Neem een kijkje op onze blog!" class="about__icon">
+                    <p class="about__text">In onze blog kunt u bijhouden wat ons interesseert in de industrie of nieuwtjes over de praktijk lezen</p>
+                </div>
+                <div class="about__carousel-item">
+                    <img src="<?php echo HTTP; ?>src/img/icons/icon_ipad.svg" alt="Voor een afspraak, bel ons!" class="about__icon">
+                    <p class="about__text">Voor het maken van een afspraak, bij voorkeur telefonisch contact opnemen</p>
+                </div>
+            </div>
         </div>
-        <div class="about__carousel-item">
-            <img src="./src/img/icons/icon_blog.svg" alt="Neem een kijkje op onze blog!" class="about__icon">
-            <p class="about__text">In onze blog kunt bijhouden wat ons interesseert in de industrie of over de werkingen in de praktijk</p>
-        </div>
-        <div class="about__carousel-item">
-            <img src="./src/img/icons/icon_ipad.svg" alt="Voor een afspraak, bel ons!" class="about__icon">
-            <p class="about__text">Voor het maken van een afspraak verzoeken wij u de praktijk via het telefoonnummer te bellen</p>
-        </div>
-    </div>
-</div>
+        
         <div class="controls controls--about">
             <img src="<?php echo HTTP; ?>src/img/icons/icon_arrow-right.svg" alt="Scroll voor meer content" class="controls__icon">
             <div class="controls__text">Scroll voor meer</div>
         </div>
+    </div>
 
-<div class="blog-container">
-    <h2 class="blog__title title">Blog</h2>
-    <div class="blog__items">
-        <div class="blog__item">
-            <h3 class="blog__headline">Paraveterinair's pleidooi na diefstal van familiehond</h3>
-            <div class="blog__excerpt">
-                <p> De Engelse bull terrier Peppa werd in december 2019 gestolen uit een tuin in Cardiff, Engeland en is nog steeds vermist, ondanks een microchip.</p>
+    <div class="blog-container" id="blog">
+        <h2 class="blog__title title">Blog</h2>
+        <div class="blog__items">
+
+            <div class="blog__item">
+                <a href="<?php echo HTTP; ?>content/blog/2020/blogpost.php" class="blog__headline-link">
+                    <h3 class="blog__headline"><?php the_title('content/blog/2020/blogpost.php'); ?></h3>
+                </a>
+                <p class="blog__excerpt"><?php the_excerpt('content/blog/2020/blogpost.php'); ?></p>
+                <a href="<?php echo HTTP; ?>content/blog/2020/blogpost.php" class="blog__readmore">> Lees meer</a>
+                <div class="blog__metadata">12 februari 2020</div>
             </div>
-            <a class="blog__readmore">> Lees meer</a>
-            <div class="blog__metadata">12 februari 2020</div>
-        </div>
-        <div class="blog__item">
-            <h3 class="blog__headline">EU-optreden tegen huisdierenpaspoorten voorgesteld</h3>
-            <div class="blog__excerpt">
-                <p>Het Europees Parlement roept op tot strengere maatregelen ter bestrijding van illegale handel in huisdieren.</p>
-            </div>
-            <a class="blog__readmore">> Lees meer</a>
-            <div class="blog__metadata">23 januari 2020</div>
-        </div>
-        <div class="blog__item">
-            <h3 class="blog__headline">Organisaties zetten konijnen in de schijnwerpers</h3>
-            <div class="blog__excerpt">
-                <p>Verscheidene organisaties hebben samengewerkt om een ​​gezamenlijke verklaring en een reeks aanbevelingen te produceren om een ​​beter welzijn van konijnen te bevorderen.</p>
-            </div>
-            <a class="blog__readmore">> Lees meer</a>
-            <div class="blog__metadata">14 januari 2020</div>
+
         </div>
     </div>
-</div>
 
-<div class="team-container">
-    <div class="team__title title">Team</div>
-    <div class="team__carousel carousel">
-        <div class="team__carousel-item">
-            <div class="team__member-name">
-                J.M.M. Graafmans
-            </div>
-            <div class="team__member-position">
-                Manager/dierenarts
-            </div>
-        </div>
-        <div class="team__carousel-item">
-            <div class="team__member-name">
-                P.A. van Baren
-            </div>
-            <div class="team__member-position">
-                Dierenarts
-            </div>
-        </div>
-        <div class="team__carousel-item">
-            <div class="team__member-name">
-                E. Meulenman &mdash; van Empel
-            </div>
-            <div class="team__member-position">
-                Dierenarts
-            </div>
-        </div>
-        <div class="team__carousel-item">
-            <div class="team__member-name">
-                J.W. Kromhout
-            </div>
-            <div class="team__member-position">
-                Dierenarts
-            </div>
-        </div>
-        <div class="team__carousel-item">
-            <div class="team__member-name">
-                K. Poppe
-            </div>
-            <div class="team__member-position">
-                Dierenarts
-            </div>
-        </div>
-        <div class="team__carousel-item">
-            <div class="team__member-name">
+    <div class="team__outer-container" id="team">
+        <div class="team__inner-container">
+            <div class="team__title title">Team</div>
+            <div class="team__carousel carousel">
+                <div class="team__carousel-item">
+                    <div class="team__member-name">
+                        J.M.M. Graafmans
+                    </div>
+                    <div class="team__member-position">
+                        Dierenarts
+                    </div>
+                </div>
+                <div class="team__carousel-item">
+                    <div class="team__member-name">
+                        P.A. van Baren
+                    </div>
+                    <div class="team__member-position">
+                        Dierenarts
+                    </div>
+                </div>
+                <div class="team__carousel-item">
+                    <div class="team__member-name">
+                        E. Meulenman &mdash; van Empel
+                    </div>
+                    <div class="team__member-position">
+                        Dierenarts
+                    </div>
+                </div>
+                <div class="team__carousel-item">
+                    <div class="team__member-name">
+                        J.W. Kromhout
+                    </div>
+                    <div class="team__member-position">
+                        Dierenarts
+                    </div>
+                </div>
+                <div class="team__carousel-item">
+                    <div class="team__member-name">
+                        K. Poppe
+                    </div>
+                    <div class="team__member-position">
+                        Dierenarts
+                    </div>
+                </div>
+                <div class="team__carousel-item">
+                    <div class="team__member-name">
                         Z. Zwiggelaar
                     </div>
                     <div class="team__member-position">
@@ -102,15 +93,17 @@
                 </div>
                 <div class="team__carousel-item">
                     <div class="team__member-name">
-                L. Raggers
-            </div>
-            <div class="team__member-position">
-                Paraveterinair
+                        L. Raggers
+                    </div>
+                    <div class="team__member-position">
+                        Paraveterinair
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
         <div class="controls">
             <img src="<?php echo HTTP; ?>/src/img/icons/icon_arrow-right.svg" alt="Scroll voor meer content" class="controls__icon">
             <div class="controls__text">Scroll voor meer</div>
         </div>
+    </div>
+</main>
