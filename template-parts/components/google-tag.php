d2c17d25e44cb356910f24730fb3c3df630cce8f
varvino <graafmans.tom@gmail.com>
2020-02-26 01:51:30 +0100

    Theme: added Google Analytics

---
+++ template-parts/components/google-tag.php
@@ -0,0 +1,8 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-158809390-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-158809390-1');
+</script>
