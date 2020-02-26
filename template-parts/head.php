9072d9a11538e1f14fbe0dd73e3f4603f91cad7e
varvino <graafmans.tom@gmail.com>
2020-02-26 01:55:31 +0100

    Theme: added canoncial url function

--- template-parts/head.php
+++ template-parts/head.php
@@ -2,8 +2,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Veterinair Centrum Genemuiden</title>
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700">
     <link rel="stylesheet" href="<?php echo HTTP; ?>dist/css/main.min.css">
+
+    <?php if (isset($canonical_url)) : ?>
+        <link rel="canonical" href="<?php echo $canonical_url; ?>">
+    <?php endif; ?>
 
     <?php include_once(ROOT . 'template-parts/components/google-tag.php'); ?>
 </head>
