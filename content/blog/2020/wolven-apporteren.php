8309976d739546e4a6c69aba28881f53f25aee07
varvino <graafmans.tom@gmail.com>
2020-02-28 11:43:39 +0100

    Blog: fixed canonical url variable

--- content/blog/2020/wolven-apporteren.php
+++ content/blog/2020/wolven-apporteren.php
@@ -3,7 +3,7 @@
 <?php
 $title = "Wetenschappers zijn onverwacht getuige van wolven die apporteren";
 $post_type = "post";
-$canoncial = 'http://www.veterinair-centrum.nl/blog/wolven-apporteren';
+$canonical_url = 'http://www.veterinair-centrum.nl/blog/wolven-apporteren';
 include(ROOT . "template-parts/head.php");
 ?>
 <?php include(ROOT . "template-parts/header.php"); ?>
