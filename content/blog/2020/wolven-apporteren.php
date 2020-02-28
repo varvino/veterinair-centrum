702befbc6f828969f89f06b47a9ad4f84400f9da
varvino <graafmans.tom@gmail.com>
2020-02-28 10:26:34 +0100

    Blog: content changes

--- content/blog/2020/wolven-apporteren.php
+++ content/blog/2020/wolven-apporteren.php
@@ -8,21 +8,21 @@ include(ROOT . "template-parts/head.php");
 <?php include(ROOT . "template-parts/header.php"); ?>
 
 <main class="h-container">
-    <h1 class="blogpost__headline">Wetenschappers zijn onverwacht getuige van wolven die apporteren</h1>
+    <h1 class="blogpost__headline">Wetenschappers zijn onverwacht getuige van wolvenjongen die apporteren</h1>
     <a href="<?php echo HTTP; ?>content/blog/archive.php" class="blogpost__date">
         17 januari 2020
     </a>
     <div class="blogpost__content">
         <p class="blogpost__content-lead">Als het gaat om apporteren, zijn veel honden natuurtalenten. Maar nu melden onderzoekers dat het opmerkelijke vermogen om menselijke sociale communicatieve signalen te begrijpen, waarmee een hond een bal kan pakken en deze vervolgens terug kan brengen - ook bij wolven bestaat.</p>
         <h2>Bij toeval ondervonden</h2>
-        <p>Tijdens een reeks onderzoeken toonden drie wolfspups spontaan interesse in een bal en keerden deze na aanmoediging terug naar een perfecte vreemdeling.</p>
-        <p>Om dit te bestuderen, fokken zij en haar team puppy's van wolven en honden vanaf de leeftijd van 10 dagen en ondergaan ze verschillende gedragstests.</p>
+        <p>Tijdens een reeks testen toonden drie wolvenjongen spontaan interesse in een bal en keerden deze na aanmoediging terug naar een onderzoeker.</p>
+        <p>Om dit te bestuderen, fokken de onderzoekers wolven met honden en ondergaan hun jongen verschillende gedragstesten.</p>
         <img src="<?php echo HTTP; ?>dist/gfx/images/2020/wolfpuppies.jpg" alt="Een foto">
         <p>De eerste twee wolvennesten waarmee ze samenwerkten, toonden weinig tot geen interesse in ballen, laat staan ​​dat ze er één haalden.</p>
-        <p>Dat is totdat ze het derde wolvenvoeder testten en sommige puppy's niet alleen voor de bal gingen, maar ook reageerden op de sociale signalen van de onbekende persoon en deze terugbrachten.</p>
+        <p>Dat is totdat ze het derde nest testten en sommige puppy's niet alleen voor de bal gingen, maar ook reageerden op de sociale signalen van de onderzoekers en deze terugbrachten.</p>
         <h2>Heel verassend</h2>
-        <p>Hoewel het een verrassing was om een ​​wolfpup te zien spelen en op die manier contact te maken met een persoon, zegt ze achteraf ook dat het logisch is.</p>
-        <p>"Wolfspups die mensgericht gedrag vertonen, kunnen een selectief voordeel hebben gehad in de vroege stadia van het domesticeren van honden", zegt ze.</p>
+        <p>"Hoewel het een verrassing was om een ​​wolfpup te zien spelen en op die manier contact te maken met een mens, is het ook logisch", aldus een onderzoeker.</p>
+        <p>"Wolvenjongen die mensgericht gedrag vertonen, kunnen een selectief voordeel hebben gehad in de vroege stadia van de domesticatie van honden", zegt een onderzoeker.</p>
     </div>
     <div class="references-container">
         <div class="references__title">Bronnen</div>
