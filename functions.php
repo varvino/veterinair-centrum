24bfe46657f32a57a5b163cd2b40d1c39dca6470
varvino <graafmans.tom@gmail.com>
2020-02-29 21:04:38 +0100

    Functions: variable naming changes

--- functions.php
+++ functions.php
@@ -1,48 +1,48 @@
 <?php
 // Get title function
-function the_title($post_type_url)
+function the_title($url)
 {
-    $post_type = file_get_contents(ROOT . $post_type_url);
+    $blogpost = file_get_contents(ROOT . $url);
     @$doc = new DOMDocument();
-    @$doc->loadHTML($post_type);
+    @$doc->loadHTML($blogpost);
     $xpath = new DomXPath($doc);
     $nodeList = $xpath->query("//*[@class='blogpost__headline']");
     $node = $nodeList->item(0);
-    $title = $node->nodeValue;
+    $blogpost_title = $node->nodeValue;
 
-    if (isset($post_type)) :
-        echo $title;
+    if (isset($blogpost)) :
+        echo $blogpost_title;
     endif;
 }
 
 // Get excerpt function
-function the_excerpt($post_type_url)
+function the_excerpt($url)
 {
-    $post_type = file_get_contents(ROOT . $post_type_url);
+    $blogpost = file_get_contents(ROOT . $url);
     @$doc = new DOMDocument();
-    @$doc->loadHTML($post_type);
+    @$doc->loadHTML($blogpost);
     $xpath = new DomXPath($doc);
     $nodeList = $xpath->query("//*[@class='blogpost__content-lead']");
     $node = $nodeList->item(0);
-    $excerpt = $node->nodeValue;
+    $blogpost_excerpt = $node->nodeValue;
 
-    if (isset($post_type)) :
-        echo $excerpt;
+    if (isset($blogpost)) :
+        echo $blogpost_excerpt;
     endif;
 }
 
 // Get date function
-function the_date($post_type_url)
+function the_date($url)
 {
-    $post_type = file_get_contents(ROOT . $post_type_url);
+    $blogpost = file_get_contents(ROOT . $url);
     @$doc = new DOMDocument();
-    @$doc->loadHTML($post_type);
+    @$doc->loadHTML($blogpost);
     $xpath = new DomXPath($doc);
     $nodeList = $xpath->query("//*[@class='blogpost__date']");
     $node = $nodeList->item(0);
-    $excerpt = $node->nodeValue;
+    $blogpost_date = $node->nodeValue;
 
-    if (isset($post_type)) :
-        echo $excerpt;
+    if (isset($blogpost)) :
+        echo $blogpost_date;
     endif;
 }
