2d2b530192c5a9b2759fa839c945e682680977fc
Varvino <graafmans.tom@gmail.com>
2020-03-31 15:41:54 +0200

    Blog: added Coronavirus item

---
+++ content/blog/2020/coronavirus-maatregelen.php
@@ -0,0 +1,32 @@
+<?php require_once("../../../config.php"); ?>
+
+<?php
+$meta_title = "Coronavirus maatregelen bij het Veterinair Centrum Genemuiden";
+$meta_description = "Vanwege de Corona-crisis hebben wij voorzorgsmaatregelen getroffen en is onze dienstverlening aangepast aan de nieuwe situatie.";
+$meta_image = "dist/gfx/images/2020/coronavirus.jpg";
+$post_type = "post";
+include(ROOT . "template-parts/head.php");
+?>
+<?php include(ROOT . "template-parts/header.php"); ?>
+
+<main class="h-container">
+    <h1 class="blogpost__headline">Coronavirus maatregelen bij het Veterinair Centrum Genemuiden</h1>
+    <a href="<?php echo HTTP; ?>content/blog/archive.php" class="blogpost__date">
+        16 maart 2020
+    </a>
+    <div class="blogpost__content">
+        <p class="blogpost__content-lead">Vanwege de Corona-crisis hebben wij voorzorgsmaatregelen getroffen en is onze dienstverlening aangepast aan de nieuwe situatie.</p>
+        <h2>COVID-19 maatregelen</h2>
+        <ul>
+            <li>de dierenartsen zullen zoveel mogelijk vanuit huis naar klanten in het buitengebied rijden.</li>
+            <li>wij zullen meerdere keren per dag de belangrijkste contactpunten (deurklinken, balie, pinapparaat) ontsmetten.</li>
+            <li>niet-urgente afspraken op de praktijk worden 3 weken verplaatst (na 6 april 2020)</li>
+            <li>respecteer zoveel mogelijk een afstand van 1-1,5 meter tussen u en de dierenartsen en assistente.</li>
+        </ul>
+        <img src="<?php echo HTTP; ?>dist/gfx/images/2020/coronavirus.jpg" alt="Coronavirus afbeelding">
+        <p>Hopelijk kunnen we met deze werkwijze iets bijdragen om verdere verspreiding van het coronavirus tegen te gaan. Het is uiteindelijk ook in uw eigen belang en dat van onze medewerkers om de gezondheid en veiligheid voorop te stellen.</p>
+        <p>Wij vinden het jammer dat we deze maatregelen moeten treffen, maar het zou fijn zijn als ze slechts van tijdelijke aard hoeven te zijn.</p>
+    </div>
+</main>
+
+<?php include(ROOT . "template-parts/footer.php"); ?>
